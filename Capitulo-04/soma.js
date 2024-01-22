05de9e9a1bf78b90234027d5c24bd52ecc09e71b
Adriele <adriele.dejprimo@gmail.com>
2024-01-22 19:06:37 -0300

    Exemplo de DC com recursão e cas-base

---
+++ Capitulo-04/soma.js
@@ -0,0 +1,13 @@
+function soma (arr){
+    if(arr.length === 1){
+        return arr[0];
+    } else{
+        const dividir = Math.floor(arr.length/2);
+
+        const primeiraParte = soma(arr.slice(0, dividir));
+        const segundaParte = soma(arr.slice(dividir));
+        return primeiraParte+segundaParte;
+    }
+}
+
+console.log(soma([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
